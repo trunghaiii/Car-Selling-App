89ca750cd0a9077671fa4211108e12cefcc6007b
trunghaiii <haitran022001@gmail.com>
2024-11-28 21:57:51 -0500

    define route controller and view for signup page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+
+Route::get('/signup', [SignupController::class,'create']);
