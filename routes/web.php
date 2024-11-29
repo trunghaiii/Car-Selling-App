9b919a35babbe7fe544d184973c04fc26c98c439
trunghaiii <haitran022001@gmail.com>
2024-11-28 23:13:12 -0500

    define route controller and view for login page

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
 
 Route::get('/signup', [SignupController::class,'create']);
+Route::get('/login', [LoginController::class,'create']);
+
